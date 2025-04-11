3305507a0010c7f2b9ed555fdad15c18a5dec1c6
Elemmire <elemmire@ossiriand.de>
2025-04-11 13:40:44 +0200

    migrations start

--- migrations/001_data.sql
+++ migrations/001_data.sql
@@ -9,3 +9,11 @@ FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"' IGNORE 2 LINES
 LOAD DATA LOCAL INFILE 'find.csv' INTO TABLE find
 FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"' IGNORE 2 LINES
 (id, bucket_id, trench, `date`, year, month, date_remarks, sca_register, `object`, object_no, category, category_no, weight, quantity, dimensions, preservation, description, material, material_remarks, dating_absolute, typology_reference, publications, remarks, rebuild_changes, created, modified);
+
+LOAD DATA LOCAL INFILE 'specialist.csv' INTO TABLE specialist
+FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"' IGNORE 1 LINES
+(id, name);
+
+LOAD DATA LOCAL INFILE 'find_specialist.csv' INTO TABLE find_specialist
+FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"' IGNORE 2 LINES
+(find_id, specialist_id, year, speciality);
