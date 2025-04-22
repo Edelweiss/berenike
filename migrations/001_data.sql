064e194f4ea0f8336bf0e3942b19b4d53b3fed95
Elemmire <elemmire@ossiriand.de>
2025-04-22 14:02:33 +0200

    migrations textile

--- migrations/001_data.sql
+++ migrations/001_data.sql
@@ -1,19 +1,23 @@
-LOAD DATA LOCAL INFILE 'locus.csv' INTO TABLE locus
+LOAD DATA LOCAL INFILE '001_locus.csv' INTO TABLE locus
 FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"' IGNORE 2 LINES
 (id, site_id, season_id, trench_id, `number`, description, created, modified);
 
-LOAD DATA LOCAL INFILE 'bucket.csv' INTO TABLE bucket
+LOAD DATA LOCAL INFILE '001_pb_bucket.csv' INTO TABLE bucket
 FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"' IGNORE 2 LINES
 (id, locus_id, `number`, dating, remarks, created, modified);
 
-LOAD DATA LOCAL INFILE 'find.csv' INTO TABLE find
+LOAD DATA LOCAL INFILE '001_find.csv' INTO TABLE find
 FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"' IGNORE 2 LINES
 (id, bucket_id, trench, `date`, year, month, date_remarks, sca_register, `object`, object_no, category, category_no, weight, quantity, dimensions, preservation, description, material, material_remarks, dating_absolute, typology_reference, publications, remarks, rebuild_changes, created, modified);
 
-LOAD DATA LOCAL INFILE 'specialist.csv' INTO TABLE specialist
+LOAD DATA LOCAL INFILE '001_specialist.csv' INTO TABLE specialist
 FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"' IGNORE 1 LINES
 (id, name);
 
-LOAD DATA LOCAL INFILE 'find_specialist.csv' INTO TABLE find_specialist
+LOAD DATA LOCAL INFILE '001_find_specialist.csv' INTO TABLE find_specialist
 FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"' IGNORE 2 LINES
 (find_id, specialist_id, year, speciality);
+
+LOAD DATA LOCAL INFILE '001_image_photo1.csv' INTO TABLE image
+FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"' IGNORE 2 LINES
+(find_id, id, type, size, file, path);
