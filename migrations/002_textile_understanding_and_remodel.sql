aff1db9561d49d9d372f691fcf9c4f00de2548c6
Elemmire <elemmire@ossiriand.de>
2025-07-02 15:57:42 +0200

    hierarchy for buckt>locus>trench

--- migrations/002_textile_understanding_and_remodel.sql
+++ migrations/002_textile_understanding_and_remodel.sql
@@ -1,3 +1,209 @@
+-- Fehlende trenches ergänzen in textile_berenike
+
+SELECT CONCAT('INSERT INTO textile_trench VALUES (', Textile_ID, ', 59);') FROM textile t LEFT JOIN `textile_trench` tt ON tt.Textile_ID11 = t.Textile_ID WHERE t.Textile_ID LIKE '%1059' AND tt.Textile_ID11 is NULL;
+INSERT INTO textile_trench VALUES (371059, 59);
+INSERT INTO textile_trench VALUES (381059, 59);
+INSERT INTO textile_trench VALUES (401059, 59);
+INSERT INTO textile_trench VALUES (411059, 59);
+INSERT INTO textile_trench VALUES (421059, 59);
+INSERT INTO textile_trench VALUES (431059, 59);
+INSERT INTO textile_trench VALUES (441059, 59);
+INSERT INTO textile_trench VALUES (451059, 59);
+INSERT INTO textile_trench VALUES (471059, 59);
+INSERT INTO textile_trench VALUES (481059, 59);
+INSERT INTO textile_trench VALUES (551059, 59);
+INSERT INTO textile_trench VALUES (561059, 59);
+INSERT INTO textile_trench VALUES (571059, 59);
+INSERT INTO textile_trench VALUES (581059, 59);
+INSERT INTO textile_trench VALUES (591059, 59);
+INSERT INTO textile_trench VALUES (601059, 59);
+INSERT INTO textile_trench VALUES (611059, 59);
+INSERT INTO textile_trench VALUES (621059, 59);
+INSERT INTO textile_trench VALUES (631059, 59);
+INSERT INTO textile_trench VALUES (641059, 59);
+INSERT INTO textile_trench VALUES (651059, 59);
+INSERT INTO textile_trench VALUES (661059, 59);
+INSERT INTO textile_trench VALUES (671059, 59);
+INSERT INTO textile_trench VALUES (681059, 59);
+INSERT INTO textile_trench VALUES (691059, 59);
+INSERT INTO textile_trench VALUES (701059, 59);
+INSERT INTO textile_trench VALUES (711059, 59);
+INSERT INTO textile_trench VALUES (721059, 59);
+INSERT INTO textile_trench VALUES (731059, 59);
+INSERT INTO textile_trench VALUES (741059, 59);
+INSERT INTO textile_trench VALUES (751059, 59);
+INSERT INTO textile_trench VALUES (761059, 59);
+INSERT INTO textile_trench VALUES (771059, 59);
+INSERT INTO textile_trench VALUES (781059, 59);
+INSERT INTO textile_trench VALUES (811059, 59);
+INSERT INTO textile_trench VALUES (821059, 59);
+INSERT INTO textile_trench VALUES (831059, 59);
+INSERT INTO textile_trench VALUES (841059, 59);
+INSERT INTO textile_trench VALUES (851059, 59);
+INSERT INTO textile_trench VALUES (861059, 59);
+INSERT INTO textile_trench VALUES (871059, 59);
+INSERT INTO textile_trench VALUES (881059, 59);
+INSERT INTO textile_trench VALUES (891059, 59);
+INSERT INTO textile_trench VALUES (901059, 59);
+INSERT INTO textile_trench VALUES (911059, 59);
+INSERT INTO textile_trench VALUES (931059, 59);
+INSERT INTO textile_trench VALUES (941059, 59);
+INSERT INTO textile_trench VALUES (951059, 59);
+INSERT INTO textile_trench VALUES (961059, 59);
+INSERT INTO textile_trench VALUES (971059, 59);
+INSERT INTO textile_trench VALUES (981059, 59);
+INSERT INTO textile_trench VALUES (991059, 59);
+INSERT INTO textile_trench VALUES (1001059, 59);
+INSERT INTO textile_trench VALUES (1011059, 59);
+INSERT INTO textile_trench VALUES (1021059, 59);
+INSERT INTO textile_trench VALUES (1031059, 59);
+INSERT INTO textile_trench VALUES (1041059, 59);
+INSERT INTO textile_trench VALUES (1051059, 59);
+INSERT INTO textile_trench VALUES (1071059, 59);
+INSERT INTO textile_trench VALUES (1081059, 59);
+INSERT INTO textile_trench VALUES (1091059, 59);
+INSERT INTO textile_trench VALUES (1101059, 59);
+INSERT INTO textile_trench VALUES (1111059, 59);
+INSERT INTO textile_trench VALUES (1121059, 59);
+INSERT INTO textile_trench VALUES (1131059, 59);
+INSERT INTO textile_trench VALUES (1141059, 59);
+INSERT INTO textile_trench VALUES (1151059, 59);
+INSERT INTO textile_trench VALUES (1161059, 59);
+INSERT INTO textile_trench VALUES (1171059, 59);
+INSERT INTO textile_trench VALUES (1191059, 59);
+INSERT INTO textile_trench VALUES (1201059, 59);
+INSERT INTO textile_trench VALUES (1211059, 59);
+INSERT INTO textile_trench VALUES (1221059, 59);
+INSERT INTO textile_trench VALUES (1231059, 59);
+INSERT INTO textile_trench VALUES (1241059, 59);
+INSERT INTO textile_trench VALUES (1251059, 59);
+INSERT INTO textile_trench VALUES (1261059, 59);
+INSERT INTO textile_trench VALUES (1281059, 59);
+INSERT INTO textile_trench VALUES (1291059, 59);
+INSERT INTO textile_trench VALUES (1301059, 59);
+INSERT INTO textile_trench VALUES (1311059, 59);
+INSERT INTO textile_trench VALUES (1321059, 59);
+INSERT INTO textile_trench VALUES (1331059, 59);
+INSERT INTO textile_trench VALUES (1341059, 59);
+INSERT INTO textile_trench VALUES (1351059, 59);
+INSERT INTO textile_trench VALUES (1361059, 59);
+INSERT INTO textile_trench VALUES (1371059, 59);
+INSERT INTO textile_trench VALUES (1381059, 59);
+INSERT INTO textile_trench VALUES (1391059, 59);
+INSERT INTO textile_trench VALUES (1401059, 59);
+INSERT INTO textile_trench VALUES (1411059, 59);
+INSERT INTO textile_trench VALUES (1421059, 59);
+INSERT INTO textile_trench VALUES (1431059, 59);
+INSERT INTO textile_trench VALUES (1441059, 59);
+INSERT INTO textile_trench VALUES (1451059, 59);
+INSERT INTO textile_trench VALUES (1461059, 59);
+INSERT INTO textile_trench VALUES (1471059, 59);
+INSERT INTO textile_trench VALUES (1511059, 59);
+INSERT INTO textile_trench VALUES (1521059, 59);
+INSERT INTO textile_trench VALUES (1531059, 59);
+INSERT INTO textile_trench VALUES (1541059, 59);
+INSERT INTO textile_trench VALUES (1551059, 59);
+INSERT INTO textile_trench VALUES (1561059, 59);
+INSERT INTO textile_trench VALUES (1571059, 59);
+INSERT INTO textile_trench VALUES (1581059, 59);
+INSERT INTO textile_trench VALUES (1601059, 59);
+INSERT INTO textile_trench VALUES (1681059, 59);
+INSERT INTO textile_trench VALUES (1701059, 59);
+INSERT INTO textile_trench VALUES (1741059, 59);
+INSERT INTO textile_trench VALUES (1781059, 59);
+INSERT INTO textile_trench VALUES (1991059, 59);
+INSERT INTO textile_trench VALUES (2741059, 59);
+INSERT INTO textile_trench VALUES (3031059, 59);
+INSERT INTO textile_trench VALUES (3251059, 59);
+INSERT INTO textile_trench VALUES (3261059, 59);
+INSERT INTO textile_trench VALUES (3271059, 59);
+INSERT INTO textile_trench VALUES (3281059, 59);
+INSERT INTO textile_trench VALUES (3291059, 59);
+INSERT INTO textile_trench VALUES (3301059, 59);
+INSERT INTO textile_trench VALUES (3311059, 59);
+INSERT INTO textile_trench VALUES (3321059, 59);
+INSERT INTO textile_trench VALUES (3331059, 59);
+INSERT INTO textile_trench VALUES (3341059, 59);
+INSERT INTO textile_trench VALUES (3351059, 59);
+INSERT INTO textile_trench VALUES (3361059, 59);
+INSERT INTO textile_trench VALUES (3371059, 59);
+INSERT INTO textile_trench VALUES (3381059, 59);
+INSERT INTO textile_trench VALUES (3391059, 59);
+INSERT INTO textile_trench VALUES (3401059, 59);
+INSERT INTO textile_trench VALUES (3411059, 59);
+INSERT INTO textile_trench VALUES (3421059, 59);
+INSERT INTO textile_trench VALUES (3431059, 59);
+INSERT INTO textile_trench VALUES (3441059, 59);
+INSERT INTO textile_trench VALUES (3451059, 59);
+INSERT INTO textile_trench VALUES (3461059, 59);
+INSERT INTO textile_trench VALUES (3471059, 59);
+INSERT INTO textile_trench VALUES (801059, 59);
+INSERT INTO textile_trench VALUES (391059, 59);
+INSERT INTO textile_trench VALUES (1061059, 59);
+INSERT INTO textile_trench VALUES (491059, 59);
+INSERT INTO textile_trench VALUES (1181059, 59);
+
+SELECT CONCAT('INSERT INTO textile_trench VALUES (', Textile_ID, ', 122);') FROM textile t LEFT JOIN `textile_trench` tt ON tt.Textile_ID11 = t.Textile_ID WHERE t.Textile_ID LIKE '%190122' AND tt.Textile_ID11 is NULL;
+INSERT INTO textile_trench VALUES (400190122, 122);
+INSERT INTO textile_trench VALUES (800190122, 122);
+INSERT INTO textile_trench VALUES (900190122, 122);
+
+SELECT CONCAT('INSERT INTO textile_trench VALUES (', Textile_ID, ', 114);') FROM textile t LEFT JOIN `textile_trench` tt ON tt.Textile_ID11 = t.Textile_ID WHERE t.Textile_ID LIKE '%180114' AND tt.Textile_ID11 is NULL;
+INSERT INTO textile_trench VALUES (700180114, 114);
+INSERT INTO textile_trench VALUES (2100180114, 114);
+
+SELECT CONCAT('INSERT INTO textile_trench VALUES (', Textile_ID, ', 120);') FROM textile t LEFT JOIN `textile_trench` tt ON tt.Textile_ID11 = t.Textile_ID WHERE t.Textile_ID LIKE '%190120' AND tt.Textile_ID11 is NULL;
+INSERT INTO textile_trench VALUES (900190120, 120);
+INSERT INTO textile_trench VALUES (1700190120, 120);
+INSERT INTO textile_trench VALUES (5100190120, 120);
+INSERT INTO textile_trench VALUES (5800190120, 120);
+INSERT INTO textile_trench VALUES (5900190120, 120);
+INSERT INTO textile_trench VALUES (6000190120, 120);
+INSERT INTO textile_trench VALUES (6100190120, 120);
+INSERT INTO textile_trench VALUES (6200190120, 120);
+INSERT INTO textile_trench VALUES (6300190120, 120);
+INSERT INTO textile_trench VALUES (6400190120, 120);
+INSERT INTO textile_trench VALUES (6500190120, 120);
+INSERT INTO textile_trench VALUES (6600190120, 120);
+INSERT INTO textile_trench VALUES (6700190120, 120);
+INSERT INTO textile_trench VALUES (6800190120, 120);
+INSERT INTO textile_trench VALUES (6900190120, 120);
+INSERT INTO textile_trench VALUES (7000190120, 120);
+INSERT INTO textile_trench VALUES (7100190120, 120);
+INSERT INTO textile_trench VALUES (7200190120, 120);
+INSERT INTO textile_trench VALUES (7300190120, 120);
+INSERT INTO textile_trench VALUES (7400190120, 120);
+INSERT INTO textile_trench VALUES (7500190120, 120);
+INSERT INTO textile_trench VALUES (7600190120, 120);
+INSERT INTO textile_trench VALUES (7800190120, 120);
+INSERT INTO textile_trench VALUES (7900190120, 120);
+INSERT INTO textile_trench VALUES (8000190120, 120);
+INSERT INTO textile_trench VALUES (8100190120, 120);
+
+-- einzelne textiles und ihre Zuordnung zu trenches fixen
+
+DELETE FROM textile WHERE Textile_ID = 0; -- ein Datensatz mit NULL-Werten in allen Spalten
+
+SELECT * FROM `textile` WHERE Textile_ID in (300150111,500180114,2700190120); -- textiles mit "falscher" Textile_ID gemäß Trench/Season-Nomenklatur
+SELECT * FROM `textile` WHERE Textile_ID in (300180111,500190120,2700200137); -- korrigierte Textile_IDs
+SELECT * FROM `textile` WHERE Textile_ID in (500180114,500190120); -- da gibt es leider beide schon
+
+-- Neues VARCHAR(512)-Feld migration_note
+UPDATE textile SET migration_note = 'Vorgefundene Textile_ID 300150111 sollte aufgrund der vorgefundenen Zuordnung zu Trench und Season 300180111 lauten.' WHERE Textile_ID = 300150111;
+UPDATE textile SET migration_note = 'Doppelzuordnung zu Trench 114 und 120. Zuordnung zu Trench 120 aufgrund der Textile_ID aufgehoben.' WHERE Textile_ID = 500180114;
+UPDATE textile SET migration_note = 'Doppelzuordnung zu Trench 120 und 137. Zuordnung zu Trench 137 aufgrund der Textile_ID aufgehoben.' WHERE Textile_ID = 2700190120;
+
+-- neues Feld trench::season setzen (z.B. 2001/2011/2012)
+
+SELECT t.id, t.site, t.number, t.year, t.Context, GROUP_CONCAT(DISTINCT l.season SEPARATOR '/') FROM `trench` t JOIN locus l ON l.trench_id = t.id GROUP BY t.id;
+UPDATE trench ut JOIN (SELECT t.id, t.site, t.number, t.year, t.Context, GROUP_CONCAT(DISTINCT l.season SEPARATOR '/') AS season FROM `trench` t JOIN locus l ON l.trench_id = t.id GROUP BY t.id) AS st ON ut.id = st.id SET ut.season = st.season;
+
+-- Doppeltrenches / Doppelloci
+
+SELECT t.Textile_ID, count(*) count_trenches, GROUP_CONCAT(tt.Trench_Number1 SEPARATOR '|') trenches FROM textile t JOIN `textile_trench` tt ON tt.Textile_ID11 = t.Textile_ID GROUP BY t.Textile_ID HAVING count_trenches > 1;
+SELECT t.Textile_ID, count(*) count_loci, GROUP_CONCAT(tl.Locus_number1 SEPARATOR '|') loci FROM textile t JOIN `textile_locus` tl ON tl.Textile_ID12 = t.Textile_ID GROUP BY t.Textile_ID HAVING count_loci > 1;
+SELECT t.Textile_ID, count(*) count_buckets, GROUP_CONCAT(tb.Pottery_bucket_number1 SEPARATOR '|') buckets FROM textile t JOIN `textile_pottery_bucket` tb ON tb.Textile_ID13 = t.Textile_ID GROUP BY t.Textile_ID HAVING count_buckets > 1;
+
 -- Bucket
 
 SELECT
@@ -13,10 +219,10 @@ SELECT
   textile.Textile_ID, b.Pottery_bucket_number1, l.Locus_number1, t.Trench_number1
 FROM textile
 JOIN textile_pottery_bucket b ON b.Textile_ID13 = textile.Textile_ID
-JOIN textile_locus l ON l.Textile_ID12 = textile.Textile_ID
-JOIN textile_trench t ON t.Textile_ID11 = textile.Textile_ID
+JOIN textile_locus l          ON l.Textile_ID12 = textile.Textile_ID
+JOIN textile_trench t         ON t.Textile_ID11 = textile.Textile_ID
 
-
+-- Textilien mit Trench, Loxus, Bucket
 SELECT
   t.Trench_number1 textile_t, l.Locus_number1 textile_l, b.Pottery_bucket_number1 textile_b, count(textile.Textile_ID) as count_textiles, GROUP_CONCAT(textile.Textile_ID SEPARATOR '|')
 FROM textile
@@ -30,8 +236,27 @@ GROUP BY
 ORDER BY textile_t, textile_l, textile_b
 LIMIT 1000
 
+-- Textilien mit Trench, Locus, Bucket, **Season**
 SELECT
-  t.number berenike_t, l.number berenike_l, b.number berenike_b, b.id berenike_b_id
+  tt.Trench_number1 textile_t, t.`Year` textile_season, l.Locus_number1 textile_l, b.Pottery_bucket_number1 textile_b, count(textile.Textile_ID) as count_textiles, GROUP_CONCAT(textile.Textile_ID SEPARATOR '|')
+FROM textile
+LEFT JOIN textile_pottery_bucket b ON b.Textile_ID13 = textile.Textile_ID
+LEFT JOIN textile_locus l ON l.Textile_ID12 = textile.Textile_ID
+LEFT JOIN textile_trench tt ON tt.Textile_ID11 = textile.Textile_ID
+LEFT JOIN trench_t t ON tt.Trench_number1 = t.Trench_number
+GROUP BY
+  b.Pottery_bucket_number1,
+  l.Locus_number1,
+  tt.Trench_number1,
+  t.Year
+ORDER BY textile_t, t.Year, textile_l, textile_b
+LIMIT 1000
+
+-- Bereninke::Buckets mit Trench, Locus, Bucket, **Season**
+
+
+SELECT
+  t.number berenike_t, l.number berenike_l, l.season berenike_season, b.number berenike_b, b.id berenike_b_id
 FROM bucket b
 JOIN locus l ON b.locus_id = l.id
 JOIN trench t ON l.trench_id = t.id
@@ -41,19 +266,88 @@ WHERE
 GROUP BY
   b.id,
   l.id,
-  t.id
-ORDER BY CAST(berenike_t AS INT), berenike_l, CAST(berenike_b AS INT)
+  t.id,
+  l.season
+ORDER BY CAST(berenike_t AS INT), berenike_season, berenike_l, CAST(berenike_b AS INT)
 LIMIT 1000
 
+-- Die obigen Selects vereint für Abgleich und später insert
 
-berenike::site mit der neuen Tabelle berenike::trench verknüpfen
+SELECT * FROM 
+(
+SELECT
+  tt.Trench_number1 textile_t, t.`Year` textile_season, l.Locus_number1 textile_l, b.Pottery_bucket_number1 textile_b, count(textile.Textile_ID) as count_textiles, GROUP_CONCAT(textile.Textile_ID SEPARATOR '|')
+FROM textile
+LEFT JOIN textile_pottery_bucket b ON b.Textile_ID13 = textile.Textile_ID
+LEFT JOIN textile_locus l ON l.Textile_ID12 = textile.Textile_ID
+LEFT JOIN textile_trench tt ON tt.Textile_ID11 = textile.Textile_ID
+LEFT JOIN trench_t t ON tt.Trench_number1 = t.Trench_number
+GROUP BY
+  b.Pottery_bucket_number1,
+  l.Locus_number1,
+  tt.Trench_number1,
+  t.Year
 
-Trench-Tabelle füllen:
+) b_text LEFT JOIN
+(
+SELECT
+  t.number berenike_t, l.number berenike_l, l.season berenike_season, b.number berenike_b, b.id berenike_b_id
+FROM berenike.bucket b
+JOIN berenike.locus l ON b.locus_id = l.id
+JOIN berenike.trench t ON l.trench_id = t.id
+WHERE
+  t.site = 'BE' AND
+  t.number in ('59', '60', '111', '112', '114', '118', '120', '122', '137')
+GROUP BY
+  b.id,
+  l.id,
+  t.id,
+  l.season
+) b_find
+ON textile_t = berenike_t AND textile_season = berenike_season AND textile_l = berenike_l AND textile_b = berenike_b
+ORDER BY
+CAST(berenike_t AS INT), berenike_season, berenike_l, CAST(berenike_b AS INT),
+  textile_t, textile_season, textile_l, textile_b
+ 
+LIMIT 1000
+
+-- berenike::site mit der neuen Tabelle berenike::trench verknüpfen
+
+-- Trench-Tabelle füllen:
 
 SELECT site, trench FROM `locus` group by site, trench ORDER BY `trench` DESC;
 
 INSERT INTO trench (site, `number`) SELECT site, trench FROM `locus` WHERE NOT trench IN ('59', '60', '111', '112', '114', '118', '120', '122', '137') group by site, trench ORDER BY `trench` DESC;
 
-Trench-Tabelle verknüpfen:
+-- Trench-Tabelle verknüpfen:
 
-UPDATE locus JOIN trench ON locus.site = trench.site AND locus.trench = trench.number SET locus.trench_id = trench.id
+UPDATE locus JOIN trench ON locus.site = trench.site AND locus.trench = trench.number SET locus.trench_id = trench.id;
+
+-- Checken, ob alle da sind [alle da, alles gut!]:
+
+SELECT l.trench, t.number, if(l.trench = t.number, 'ok', 'not ok') as check_trench FROM `locus` l LEFT JOIN trench t ON l.trench_id = t.id WHERE 1 ORDER BY `check_trench` ASC;
+
+-- Aktuelles Arbeit / Workbench
+
+-- Bucket-Identitäten feststellen
+
+SELECT Textile_ID, b.id, b.tmp_id, b.Pottery_bucket_number1, l.id, l.tmp_id, l.Locus_number1, tt.Trench_number1
+FROM `textile` t LEFT JOIN textile_pottery_bucket b ON b.Textile_ID13 = t.Textile_ID LEFT JOIN textile_locus l ON l.Textile_ID12 = t.Textile_ID LEFT JOIN textile_trench tt ON tt.Textile_ID11 = t.Textile_ID
+WHERE 1  
+ORDER BY Pottery_bucket_number1
+LIMIT 1000
+
+-- Tmp-Id für Buckets generieren
+
+UPDATE `textile` t LEFT JOIN textile_pottery_bucket b ON b.Textile_ID13 = t.Textile_ID LEFT JOIN textile_locus l ON l.Textile_ID12 = t.Textile_ID LEFT JOIN textile_trench tt ON tt.Textile_ID11 = t.Textile_ID
+SET b.tmp_id = Concat(Pottery_bucket_number1, '_', Locus_number1, '_', tt.Trench_number1)
+
+-- Buckets mit Id generieren
+
+
+SELECT Textile_ID13 AS textile_id, b.id as bucket_id, b.tmp_id as bucket_tmp, Pottery_bucket_number as bucket_number
+FROM textile_pottery_bucket b LEFT JOIN pottery_bucket ON Pottery_bucket_number = Pottery_bucket_number1
+ORDER BY tmp_id
+LIMIT 1000
+
+
