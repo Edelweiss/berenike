532895ea8a4a42d813f3f10b8087b9c6a3884578
Elor Oké (Piccolo) <elor.oke@adamsakte.de>
2025-11-04 11:25:45 +0100

    add dashboard

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,12 +1,40 @@
 <?php
 
 namespace App\Controller;
+
+use App\Repository\FindRepository;
 use Symfony\Component\HttpFoundation\Response;
+use Doctrine\ORM\EntityManagerInterface;
 
 class HomeController extends BerenikeController{
+    private $entityManager;
+    private $findRepository;
+
+    public function __construct(
+        \Symfony\Component\HttpFoundation\RequestStack $requestStack,
+        \Psr\Log\LoggerInterface $logger,
+        EntityManagerInterface $entityManager,
+        FindRepository $findRepository
+    ) {
+        parent::__construct($requestStack, $logger);
+        $this->entityManager = $entityManager;
+        $this->findRepository = $findRepository;
+    }
+
     public function index(): Response {
         return $this->render('home/index.html.twig');
     }
+
+    public function dashboard(): Response {
+        // Fetch specific Find records
+        $findIds = [20742, 27904, 28416];
+        $finds = $this->findRepository->findBy(['id' => $findIds]);
+
+        return $this->render('home/dashboard.html.twig', [
+            'finds' => $finds
+        ]);
+    }
+
     public function about(): Response {
         return $this->render('home/about.html.twig');
     }
