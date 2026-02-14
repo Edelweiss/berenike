3ea75aedbf1ae812f25a3294e538511b46698951
Elor Oké (Piccolo) <elor.oke@adamsakte.de>
2026-02-14 02:20:52 +0100

    new buckets for marina_3

--- migrations/010_marina3_check_buckets.sql
+++ migrations/010_marina3_check_buckets.sql
@@ -76,3 +76,40 @@ Number of loci for new buckets to add that cannot be matched to a locus: 1 (BE44
 -- -1 because of duplicate bucket number (2595, 1)
 -- -1 because of span in field pb_pb_no (in Marina_3 002+004)
 -- = 579 new bucket entries to add
+
+
+-- check iwona_3 authority:
+
+SELECT * FROM
+
+(SELECT f.loci_site_id as site, concat(IF(f.loci_season_id > 50, '19', '20'), f.loci_season_id) as season, REGEXP_REPLACE(f.loci_trench_id, '^0*(.+)$', '\\1') as trench, CONCAT_WS('', f.loci_locusno+0, LOWER(REGEXP_SUBSTR(f.loci_locusno, '[A-Za-z]'))) as locus, REGEXP_REPLACE(REGEXP_REPLACE(f.pb_pb_no, '^0*(.+)$', '\\1'), '/0+', '/') as bucket, f.id FROM marina_3 f JOIN import on import.id = f.id and authority LIKE 'marina_3%') f
+
+LEFT JOIN (SELECT e.id excavation_id, e.site, e.season, e.trench, CONCAT_WS('', l.number, LOWER(l.addendum)) as locus, b.number as bucket, b.id as bucket_id FROM bucket b JOIN locus l ON b.locus_id = l.id JOIN excavation e ON l.excavation_id = e.id group by e.id, site, season, trench, locus, bucket) b
+ON  f.site = b.site
+AND LOCATE(f.season, b.season) > 0
+AND f.trench = b.trench
+AND f.locus = b.locus
+AND f.bucket = b.bucket
+
+WHERE f.site = 'BE' AND f.season is not null AND f.trench is not null AND f.locus is not null AND f.bucket is not null AND b.bucket_id is null
+ORDER BY f.site, f.season, f.trench, f.locus, f.bucket;
+
+-- 20468 - 20347 = 121 finds that cannot be matched to a bucket
+
+-- create missing bucket numbers:
+
+SELECT * FROM
+
+(SELECT f.loci_site_id as site, concat(IF(f.loci_season_id > 50, '19', '20'), f.loci_season_id) as season, REGEXP_REPLACE(f.loci_trench_id, '^0*(.+)$', '\\1') as trench, CONCAT_WS('', f.loci_locusno+0, LOWER(REGEXP_SUBSTR(f.loci_locusno, '[A-Za-z]'))) as locus, REGEXP_REPLACE(REGEXP_REPLACE(f.pb_pb_no, '^0*(.+)$', '\\1'), '/0+', '/') as bucket, count(f.id) as num_finds FROM marina_3 f JOIN import on import.id = f.id and authority LIKE 'marina_3%' group by f.loci_site_id, f.loci_season_id, f.loci_trench_id, f.loci_locusno, f.pb_pb_no) f
+
+LEFT JOIN (SELECT e.id excavation_id, e.site, e.season, e.trench, CONCAT_WS('', l.number, LOWER(l.addendum)) as locus, b.number as bucket, b.id as bucket_id FROM bucket b JOIN locus l ON b.locus_id = l.id JOIN excavation e ON l.excavation_id = e.id group by e.id, site, season, trench, locus, bucket) b
+ON  f.site = b.site
+AND LOCATE(f.season, b.season) > 0
+AND f.trench = b.trench
+AND f.locus = b.locus
+AND f.bucket = b.bucket
+
+WHERE f.site = 'BE' AND f.season is not null AND f.trench is not null AND f.locus is not null AND f.bucket is not null AND b.bucket_id is null
+ORDER BY f.site, f.season, f.trench, f.locus, f.bucket;
+
+-- 23 missing buckets, all with bs and as and xx.s and stuff
