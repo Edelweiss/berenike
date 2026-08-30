48c6e1ef4b05ff234b50604cebae9f232c9977f9
Elor Oké (Piccolo) <elor.oke@adamsakte.de>
2026-08-30 10:59:41 +0200

    check for non-BE data

--- migrations/012_iwona3_check_buckets.sql
+++ migrations/012_iwona3_check_buckets.sql
@@ -5,7 +5,7 @@ SELECT f.loci_site_id as site, concat(IF(f.loci_season_id > 50, '19', '20'), f.l
 -- All buckets in iwona_3 finds not matching existing buckets
 SELECT * FROM
 
-(SELECT i.loci_site_id as site, concat(IF(i.loci_season_id > 50, '19', '20'), i.loci_season_id) as season, REGEXP_REPLACE(i.loci_trench_id, '^0*(.+)$', '\\1') as trench, CONCAT_WS('', loci_locusno+0, LOWER(REGEXP_SUBSTR(loci_locusno, '[A-Za-z]'))) as locus, REGEXP_REPLACE(f.pb_pb_no, '^0*(.+)$', '\\1') as bucket, count(*) as finds FROM iwona_3 i JOIN import on import.id = i.id and authority = 'iwona_3' GROUP BY site, season, trench, locus, bucket) f
+(SELECT i.loci_site_id as site, concat(IF(i.loci_season_id > 50, '19', '20'), i.loci_season_id) as season, REGEXP_REPLACE(i.loci_trench_id, '^0*(.+)$', '\\1') as trench, CONCAT_WS('', loci_locusno+0, LOWER(REGEXP_SUBSTR(loci_locusno, '[A-Za-z]'))) as locus, REGEXP_REPLACE(i.pb_pb_no, '^0*(.+)$', '\\1') as bucket, count(*) as finds FROM iwona_3 i JOIN import on import.id = i.id and authority = 'iwona_3' GROUP BY site, season, trench, locus, bucket) f
 
 LEFT JOIN (SELECT e.id excavation_id, e.site, e.season, e.trench, CONCAT_WS('', l.number, LOWER(l.addendum)) as locus, b.number as bucket FROM bucket b JOIN locus l ON b.locus_id = l.id JOIN excavation e ON l.excavation_id = e.id group by e.id, site, season, trench, locus, bucket) b
 ON  f.site = b.site
@@ -14,7 +14,7 @@ AND f.trench = b.trench
 AND f.locus = b.locus
 AND f.bucket = b.bucket
 
-WHERE f.site = 'BE' AND f.season is not null AND f.trench is not null AND f.locus is not null AND f.bucket is not null AND b.excavation_id is NULL
+WHERE f.site != 'BE' AND f.season is not null AND f.trench is not null AND f.locus is not null AND f.bucket is not null AND b.excavation_id is NOT NULL
 ORDER BY f.site, f.season, f.trench, f.locus, f.bucket;
 
 -- Total: 6963
